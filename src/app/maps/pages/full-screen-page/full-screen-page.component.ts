fc826157da456c0d846fa0fec37799d2b26c1f6a
MuhamadIffatullathoif <iffatlathoif1997@gmail.com>
2024-01-21 12:21:53 +0700

    06. ViewChild Element Ref

--- src/app/maps/pages/full-screen-page/full-screen-page.component.ts
+++ src/app/maps/pages/full-screen-page/full-screen-page.component.ts
@@ -1,4 +1,4 @@
-import {AfterViewInit, Component} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, ViewChild} from '@angular/core';
 import * as mapboxgl from 'mapbox-gl';
 
 // (mapboxgl as any).accessToken = 'pk.eyJ1IjoiaWZmYXQxOTk3IiwiYSI6ImNscm16cGw4NzExcGQyanB3NHQ1ajNlbnEifQ.wPAAN7tQBA8Lk7881JaSBw';
@@ -6,12 +6,16 @@ import * as mapboxgl from 'mapbox-gl';
   templateUrl: './full-screen-page.component.html',
   styleUrl: './full-screen-page.component.css'
 })
-export class FullScreenPageComponent implements AfterViewInit{
+export class FullScreenPageComponent implements AfterViewInit {
+
+  @ViewChild('map')
+  public divMap?: ElementRef;
 
   ngAfterViewInit(): void {
+    if (!this.divMap) throw new Error("HTML Element not found");
     const map = new mapboxgl.Map({
       accessToken: 'pk.eyJ1IjoiaWZmYXQxOTk3IiwiYSI6ImNscm16cGw4NzExcGQyanB3NHQ1ajNlbnEifQ.wPAAN7tQBA8Lk7881JaSBw',
-      container: 'map', // container ID
+      container: this.divMap.nativeElement, // container ID
       style: 'mapbox://styles/mapbox/streets-v12', // style URL
       center: [-74.5, 40], // starting position [lng, lat]
       zoom: 9, // starting zoom
