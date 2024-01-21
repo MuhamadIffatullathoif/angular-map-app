57449deabb08147b319af8bfac8e6cd2f70c2cfe
MuhamadIffatullathoif <iffatlathoif1997@gmail.com>
2024-01-21 13:00:12 +0700

    08. Center Zoom Level

--- src/app/maps/pages/zoom-range-page/zoom-range-page.component.ts
+++ src/app/maps/pages/zoom-range-page/zoom-range-page.component.ts
@@ -5,18 +5,46 @@ import * as mapboxgl from "mapbox-gl";
   templateUrl: './zoom-range-page.component.html',
   styleUrl: './zoom-range-page.component.css'
 })
-export class ZoomRangePageComponent implements AfterViewInit{
+export class ZoomRangePageComponent implements AfterViewInit {
   @ViewChild('map')
   public divMap?: ElementRef;
+  public zoom: number = 10;
+  public map?: mapboxgl.Map;
 
   ngAfterViewInit(): void {
     if (!this.divMap) throw new Error("HTML Element not found");
-    const map = new mapboxgl.Map({
+    this.map = new mapboxgl.Map({
       accessToken: 'pk.eyJ1IjoiaWZmYXQxOTk3IiwiYSI6ImNscm16cGw4NzExcGQyanB3NHQ1ajNlbnEifQ.wPAAN7tQBA8Lk7881JaSBw',
       container: this.divMap.nativeElement, // container ID
       style: 'mapbox://styles/mapbox/streets-v12', // style URL
       center: [-74.5, 40], // starting position [lng, lat]
-      zoom: 9, // starting zoom
+      zoom: this.zoom, // starting zoom
     });
+    this.mapListener();
+  }
+
+  mapListener(): void {
+    if (!this.map) throw new Error("Map not initialized");
+    this.map.on('zoom', (ev) => {
+      this.zoom = this.map!.getZoom();
+    })
+
+    this.map.on('zoomend', (ev) => {
+      if(this.map!.getZoom() < 18) return;
+      this.map!.zoomTo(18);
+    })
+  }
+
+  zoomIn(): void {
+    this.map?.zoomIn();
+  }
+
+  zoomOut(): void {
+    this.map?.zoomOut();
+  }
+
+  zoomChanged(value: string) {
+    this.zoom = +value;
+    this.map?.zoomTo(this.zoom);
   }
 }
