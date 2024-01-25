9a41113c014182d5c36ec050bc2be1a1b2ef84c7
MuhamadIffatullathoif <iffatlathoif1997@gmail.com>
2024-01-25 18:00:31 +0700

    18. Standalone as Page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 
 const routes: Routes = [
   {
     path: 'maps',
     loadChildren: () => import('./maps/maps.module').then(m => m.MapsModule),
+  },
+  {
+    path: 'alone',
+    loadComponent: () => import('./alone/pages/alone-page/alone-page.component')
+      .then(m => m.AlonePageComponent)
   },
   {
     path: '**',
@@ -16,4 +21,5 @@ const routes: Routes = [
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
